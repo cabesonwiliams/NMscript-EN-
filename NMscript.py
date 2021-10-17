ce787621ec809338d8ad8d26bbec89180646955c
cabeson sin z <86396046+cabesonwiliams@users.noreply.github.com>
2021-10-17 19:02:34 -0400

    Update NMscript.py

--- NMscript.py
+++ NMscript.py
@@ -366,7 +366,7 @@ def script():
         |_________________________________________________________________________________________|        
     ''')
     print(banner)
-    opcion = input("\033[1;36m"+' Seleciona una opcion\n >> ')     
+    opcion = input("\033[1;36m"+' Select an option\n >> ')     
 
     if opcion == "001":
         print('Disclaimer: This can take a long time as it is a brute force attack ')
